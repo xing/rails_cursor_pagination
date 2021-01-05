3c2d80e02314341257c717fef4246701e4536c1e
Nicolas Fricke <mail@nicolasfricke.com>
2021-01-05 18:43:29 +0100

    Accept params on initialization of Paginator
    These params are not yet used by the class, but help us define the
    future API of the class. Also, add validations to ensure that all params
    are correct and that only the right combinations are accepted.

--- lib/rails_cursor_pagination/paginator.rb
+++ lib/rails_cursor_pagination/paginator.rb
@@ -7,20 +7,108 @@ module RailsCursorPagination
   #
   # Usage:
   #     RailsCursorPagination::Paginator
-  #       .new(relation)
+  #       .new(relation, order_by: :author, first: 2, after: "WyJKYW5lIiw0XQ==")
   #       .records
   #
   class Paginator
-    # @param [ActiveRecord::Relation] relation
-    def initialize(relation)
+    class ParameterError < Error; end
+
+    DEFAULT_PAGE_SIZE = 5
+
+    # Create a new instance of the `RailsCursorPagination::Paginator`
+    #
+    # @param relation [ActiveRecord::Relation]
+    #   Relation that will be paginated.
+    # @param first [Integer, nil]
+    #   Number of records to return in a forward pagination. Can be combined
+    #   with `after`.
+    # @param after [String, nil]
+    #   Cursor to paginate forward from. Can be combined with `first`.
+    # @param last [Integer, nil]
+    #   Number of records to return. Must be used together with `before`.
+    # @param before [String, nil]
+    #   Cursor to paginate upto (excluding). Can be combined with `last`.
+    # @param order_by [Symbol, String]
+    #   Column to order by. If none is provided, will default to ID column.
+    #   NOTE: this will cause an SQL `CONCAT` query. Therefore, you might want
+    #   to add an index to your database: `CONCAT(<order_by_field>, '-', id)`
+    # @param order [Symbol]
+    #   Ordering to apply, either `:asc` or `:desc`. Defaults to `:asc`.
+    #
+    # @raise [RailsCursorPagination::Paginator::ParameterError]
+    #   If any parameter is not valid
+    def initialize(relation, first: nil, after: nil, last: nil, before: nil,
+                   order_by: :id, order: :asc)
+      ensure_valid_params!(relation, first, after, last, before, order)
+
+      @order_field = order_by
+      @order_direction = order
       @relation = relation
+
+      @cursor = before || after
+      @is_forward_pagination = before.blank?
+
+      @page_size =
+        first ||
+        last ||
+        DEFAULT_PAGE_SIZE
     end
 
     # Get all records of the current page
     #
     # @return [Array<ActiveRecord>]
     def records
-      @relation.first(5)
+      @relation.first(@page_size)
+    end
+
+    private
+
+    # Ensure that the parameters of this service are valid. Otherwise raise
+    # a `RailsCursorPagination::Paginator::ParameterError`.
+    #
+    # @param relation [ActiveRecord::Relation]
+    #   Relation that will be paginated.
+    # @param first [Integer, nil]
+    #   Optional, must be positive, cannot be combined with `last`
+    # @param after [String, nil]
+    #   Optional, cannot be combined with `before`
+    # @param last [Integer, nil]
+    #   Optional, must be positive, requires `before`, cannot be combined
+    #   with `first`
+    # @param before [String, nil]
+    #   Optional, cannot be combined with `after`
+    # @param order [Symbol]
+    #   Optional, must be :asc or :desc
+    #
+    # @raise [RailsCursorPagination::Paginator::ParameterError]
+    #   If any parameter is not valid
+    def ensure_valid_params!(relation, first, after, last, before, order)
+      unless relation.is_a?(ActiveRecord::Relation)
+        raise ParameterError,
+              'The first argument must be an ActiveRecord::Relation, but was '\
+              "the #{relation.class} `#{relation.inspect}`"
+      end
+      unless %i[asc desc].include?(order)
+        raise ParameterError,
+              "`order` must be either :asc or :desc, but was `#{order}`"
+      end
+      if first.present? && last.present?
+        raise ParameterError, '`first` cannot be combined with `last`'
+      end
+      if before.present? && after.present?
+        raise ParameterError, '`before` cannot be combined with `after`'
+      end
+      if last.present? && before.blank?
+        raise ParameterError, '`last` must be combined with `before`'
+      end
+      if first.present? && first.negative?
+        raise ParameterError, "`first` cannot be negative, but was `#{first}`"
+      end
+      if last.present? && last.negative?
+        raise ParameterError, "`last` cannot be negative, but was `#{last}`"
+      end
+
+      true
     end
   end
 end
--- spec/rails_cursor_pagination/paginator_spec.rb
+++ spec/rails_cursor_pagination/paginator_spec.rb
@@ -1,23 +1,148 @@
 # frozen_string_literal: true
 
 RSpec.describe RailsCursorPagination::Paginator do
-  subject(:instance) { described_class.new(relation) }
-  let!(:post_1) { Post.create! id: 1, author: 'Jane' }
-  let!(:post_2) { Post.create! id: 2, author: 'John' }
-  let!(:post_3) { Post.create! id: 3, author: 'John' }
-  let!(:post_4) { Post.create! id: 4, author: 'Jane' }
-  let!(:post_5) { Post.create! id: 5, author: 'Jane' }
-  let!(:post_6) { Post.create! id: 6, author: 'John' }
-  let!(:post_7) { Post.create! id: 7, author: 'John' }
+  subject(:instance) { described_class.new(relation, **params) }
 
   let(:relation) { Post.all }
+  let(:params) { {} }
+
+  describe '.new' do
+    context 'when passing valid parameters' do
+      shared_examples 'for a working combination with `order` param' do
+        context 'and custom order' do
+          context 'set to ascending' do
+            let(:params) { super().merge(order: :asc) }
+
+            it { is_expected.to be_a described_class }
+          end
+
+          context 'set to descending' do
+            let(:params) { super().merge(order: :desc) }
+
+            it { is_expected.to be_a described_class }
+          end
+        end
+      end
+
+      shared_examples 'for a working combination with `order_by` param' do
+        context 'and custom order_by' do
+          let(:params) { super().merge(order_by: :author) }
+
+          it { is_expected.to be_a described_class }
+
+          include_examples 'for a working combination with `order` param'
+        end
+      end
+
+      shared_examples 'for a working parameter combination' do
+        it { is_expected.to be_a described_class }
+
+        include_examples 'for a working combination with `order` param'
+        include_examples 'for a working combination with `order_by` param'
+      end
+
+      context 'when only passing the relation' do
+        include_examples 'for a working parameter combination'
+      end
+
+      context 'when passing only first' do
+        let(:params) { { first: 2 } }
+
+        include_examples 'for a working parameter combination'
+      end
+
+      context 'when passing first and after' do
+        let(:params) { { first: 2, after: 'abc' } }
+
+        include_examples 'for a working parameter combination'
+      end
+
+      context 'when passing last and before' do
+        let(:params) { { last: 2, before: 'xyz' } }
+
+        include_examples 'for a working parameter combination'
+      end
+    end
+
+    context 'when passing invalid parameters' do
+      shared_examples 'for a ParameterError with the right message' do |message|
+        it 'raises an error with the right message' do
+          expect { subject }
+            .to raise_error ::RailsCursorPagination::Paginator::ParameterError,
+                            message
+        end
+      end
+
+      context 'not passing an ActiveRecord::Relation as first argument' do
+        let(:relation) { :tasty_cookies }
+
+        include_examples 'for a ParameterError with the right message',
+                         'The first argument must be an '\
+                         'ActiveRecord::Relation, but was the Symbol '\
+                         '`:tasty_cookies`'
+      end
+
+      context 'passing an invalid `order`' do
+        let(:params) { super().merge(order: :happiness) }
+
+        include_examples 'for a ParameterError with the right message',
+                         '`order` must be either :asc or :desc, but was'\
+                         ' `happiness`'
+      end
+
+      context 'passing both `first` and `last`' do
+        let(:params) { super().merge(first: 2, last: 3) }
+
+        include_examples 'for a ParameterError with the right message',
+                         '`first` cannot be combined with `last`'
+      end
+
+      context 'passing both `before` and `after`' do
+        let(:params) { super().merge(before: 'qwe', after: 'asd') }
+
+        include_examples 'for a ParameterError with the right message',
+                         '`before` cannot be combined with `after`'
+      end
+
+      context 'passing only `last` without `after`' do
+        let(:params) { super().merge(last: 5) }
+
+        include_examples 'for a ParameterError with the right message',
+                         '`last` must be combined with `before`'
+      end
+
+      context 'passing a negative `first`' do
+        let(:params) { super().merge(first: -7) }
+
+        include_examples 'for a ParameterError with the right message',
+                         '`first` cannot be negative, but was `-7`'
+      end
+
+      context 'passing a negative `last`' do
+        let(:params) { super().merge(last: -4, before: 'qwe') }
+
+        include_examples 'for a ParameterError with the right message',
+                         '`last` cannot be negative, but was `-4`'
+      end
+    end
+  end
 
   describe '#records' do
     subject { instance.records }
 
+    let(:post_1) { Post.create! id: 1, author: 'Jane' }
+    let(:post_2) { Post.create! id: 2, author: 'John' }
+    let(:post_3) { Post.create! id: 3, author: 'John' }
+    let(:post_4) { Post.create! id: 4, author: 'Jane' }
+    let(:post_5) { Post.create! id: 5, author: 'Jane' }
+    let(:post_6) { Post.create! id: 6, author: 'John' }
+    let(:post_7) { Post.create! id: 7, author: 'John' }
+
+    let!(:posts) { [post_1, post_2, post_3, post_4, post_5, post_6, post_7] }
+
     it 'returns the first 5 records' do
       is_expected.to be_an Array
-      expect(subject.size).to eq 5
+      expect(subject.size).to eq described_class::DEFAULT_PAGE_SIZE
       expect(subject).to contain_exactly post_1, post_2, post_3, post_4, post_5
     end
   end
