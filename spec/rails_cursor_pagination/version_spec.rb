9c593841a89732af5db59046accaf812d949b1e6
Nicolas Fricke <mail@nicolasfricke.com>
2021-01-12 14:50:07 +0100

    Improve versionioning of this gem
    Ruby's gems follow semantic versioning in terms of major, minor, and
    patch-level numbers. But where the semantic versioning spec 2.0.0 found
    on https://semver.org/spec/v2.0.0.html says that a version number can be
    suffixed by a hyphen followed by an alphanumeric string to e.g.
    denominate `alpha` or `pre` release versions, gems, when built using
    `bundler` substitute any hyphen with `.pre.`.
    
    So the semantic version `0.1.0-pre` defined in
    RailsCursorPagination::VERSION would then be converted to
    `0.1.0.pre.pre`. Since this is clearly not what is wanted, ensure that
    the version numbers used by this gem always use a dot to delimit the
    optional alphanumeric denominator.
    
    RubyGems considers any version number that contains characters other
    than integers and periods as pre-releases.

--- spec/rails_cursor_pagination/version_spec.rb
+++ spec/rails_cursor_pagination/version_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RailsCursorPagination::VERSION do
   it { is_expected.not_to be nil }
   it { is_expected.to be_a String }
 
-  it 'uses semantic versioning' do
-    is_expected.to match(/^\d+.\d+.\d+(-[\w\d]+)$/)
+  it 'uses gem-flavored semantic versioning' do
+    is_expected.to match(/^\d+.\d+.\d+(.[\w\d]+)$/)
   end
 end
