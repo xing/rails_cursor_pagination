87072743192a98c13b68f35478b68bcc45f23645
Nicolas Fricke <mail@nicolasfricke.com>
2021-02-03 16:04:08 +0100

    Fix version spec validator
    This spec enforced the use of a version number suffix. This is not what
    we want, that part should only be optional. Therefore, make the last
    part of the clause optional.

--- spec/rails_cursor_pagination/version_spec.rb
+++ spec/rails_cursor_pagination/version_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe RailsCursorPagination::VERSION do
   it { is_expected.to be_a String }
 
   it 'uses gem-flavored semantic versioning' do
-    is_expected.to match(/^\d+.\d+.\d+(.[\w\d]+)$/)
+    is_expected.to match(/^\d+.\d+.\d+(.[\w\d]+)?$/)
   end
 end
